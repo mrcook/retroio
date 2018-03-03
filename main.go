2dff789d524147b54aa25fa55e21d16b00be7e02
Michael R. Cook <git@mrcook.uk>
2018-03-03 18:28:24 +0100

    Spectrumator

---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+	"log"
+	"os"
+)
+
+type TzxHeader struct {
+	Signature    [7]byte
+	_            uint8 // End of file marker
+	MajorVersion uint8
+	MinorVersion uint8
+}
+
+func main() {
+	filename := "tape-file.tzx"
+
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatal("Error while opening file", err)
+	}
+	defer file.Close()
+
+	fmt.Printf("%s opened successfully!\n", filename)
+
+	header := TzxHeader{}
+	data := readNextBytes(file, 10)
+
+	buffer := bytes.NewBuffer(data)
+	err = binary.Read(buffer, binary.LittleEndian, &header)
+	if err != nil {
+		log.Fatal("binary.Read failed", err)
+	}
+
+	if string(header.Signature[:]) != "ZXTape!" {
+		log.Fatal("given TZX file is not in correct format")
+	}
+
+	fmt.Printf("TZX v%d.%d\n", header.MajorVersion, header.MinorVersion)
+}
+
+func readNextBytes(file *os.File, number int) []byte {
+	b := make([]byte, number)
+	_, err := file.Read(b)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return b
+}
